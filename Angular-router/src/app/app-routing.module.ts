49558cdd2f097661f8756312ca2b5ccd69f056c9
jairo-commit <jairoamr@gmail.com>
2025-04-24 15:58:07 -0500

    Deploy en Netlify del curso de Angular Router: Lazy Loading y Programación Modular

---
+++ Angular-router/src/app/app-routing.module.ts
@@ -0,0 +1,28 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes, PreloadAllModules } from '@angular/router';
+
+import { NotFoundComponent } from './not-found/not-found.component';
+import { CustomPreloadService } from './services/custom-preload.service';
+
+const routes: Routes = [
+  {
+    path: '',
+    loadChildren: () => import('./website/website.module').then(m => m.WebsiteModule)
+  },
+  {
+    path: 'cms',
+    loadChildren: () => import('./cms/cms.module').then(m => m.CmsModule)
+  },
+  {
+    path: '**',
+    component: NotFoundComponent
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes, {
+    preloadingStrategy: CustomPreloadService,
+  })],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
